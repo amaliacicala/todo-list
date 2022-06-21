2a0941985fcd5cb5e265bb6bcef2b005f5442e93
Amalia <cicala.amalia@gmail.com>
2022-06-21 21:53:25 +0200

    feat: clean up index.html

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons'
 export function Footer() {
     return (
         <footer>
-            <p>Made with <FontAwesomeIcon icon={faHeart} /> by <a href="https://github.com/amaliacicala">Amalia Cicala</a></p>
+            <p>Made with <FontAwesomeIcon icon={faHeart} style={{margin: '0 0.2rem'}} /> by <a href="https://github.com/amaliacicala">Amalia Cicala</a></p>
         </footer>
     )
 }
